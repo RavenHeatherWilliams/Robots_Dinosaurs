5c1853b528d5a1abc239fa703cd67352a8aa1338
Raven Williams <heatherwilliams.raven@gmail.com>
2022-08-13 21:18:39 -0500

    Removed self.attack and added self.weapon.attack_power

--- battlefield.py
+++ battlefield.py
@@ -21,24 +21,20 @@ class Battlefield:
     def battle_phase(self):
         self.dinosaur.attack(self.robot)
         self.robot.attack(self.dinosaur)
-        battle_phase = False
-        while battle_phase == (False):
-            if self.robot == "10":
-                 print(f'{self.robot} was attacked by {self.name} and did {self.attack_power} damaged leaving  the robot with {self.health} health')
-                 return battle_phase
-            elif self.robot == "0":
-                print(f' You have claimed the victory!')
-
-    def battle_phase(self):
-        self.dinosaur.attack(self.robot)
-        self.robot.attack(self.dinosaur)
-        battle_phase = False
-        while battle_phase == (False):
-            if self.dinosaur == "10":
-                 print(f'{self.dinosaur} was attacked by {self.name} and did {self.attack_power} damaged leaving  the dinosaur with {self.health} health')
-                 return battle_phase
-            elif self.dinosaur == "0":
-                print(f'You have claimed the victory!')
+      
+       
+            
+              
+    # def battle_phase(self):
+    #     self.dinosaur.attack(self.robot)
+    #     self.robot.attack(self.dinosaur)
+    #     battle_phase = False
+    #     while battle_phase == (False):
+    #         if self.dinosaur == "10":
+    #              print(f'{self.dinosaur} was attacked by {self.name} and did {self.attack_power} damaged leaving  the dinosaur with {self.health} health')
+    #              return battle_phase
+    #         elif self.dinosaur == "0":
+    #             print(f'You have claimed the victory!')
 
     def display_winner():
         battle_phase = False
--- dinosaur.py
+++ dinosaur.py
@@ -6,4 +6,4 @@ class Dinosaur:
 
     def attack(self,robot):
         robot.health -= self.attack_power
-        print(f'{self.robot} was attacked by {self.name} and did {self.attack_power} damaged leaving  the robot with {robot.health} health')
+        print(f'{robot.name} was attacked by {self.name} and did {self.attack_power} damaged leaving  the robot with {robot.health} health')
